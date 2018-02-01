0c7fb35bfb7ced4aab6dfca8d0bb042def70fb16
Kayman <blastenny@yahoo.com>
2018-02-01 15:45:12 +0100

    new class added

---
+++ src/com/javaplus/KamalClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaplus;
+
+/**
+ *
+ * @author kayman pc
+ */
+public class KamalClass {
+   
+    
+    public int getSum(int a, int b){
+ 
+        
+        return a + b;
+    }
+    
+}
