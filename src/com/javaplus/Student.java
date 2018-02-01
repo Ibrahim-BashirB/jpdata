3ff01917aa793b94c0f4e7275275fedb29261b48
jesse <ibrahim.bitrus.b@gmail.com>
2018-02-01 15:44:16 +0100

    student class added by ibrahim

---
+++ src/com/javaplus/Student.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaplus;
+
+/**
+ *
+ * @author Jesse-pc
+ */
+public class Student {
+
+    String name;
+    
+    public Student() {
+        
+        name = "kamal";
+    }
+    
+    
+    
+    public static void main(){
+        Student std = new Student();
+        System.out.println("hello student");
+    }
+    
+}
